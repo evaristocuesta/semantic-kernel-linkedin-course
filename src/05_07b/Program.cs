cd80baad3841b52e2db2afe34ee1a840133cf5f3
evaristocuesta <evaristocg@gmail.com>
2024-07-26 15:42:47 +0200

    Add 05_07

--- src/05_07b/Program.cs
+++ src/05_07b/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _05_07b;
+
+Console.WriteLine("Hello, Semantic Kernel Agents Collaboration!");
+
+try
+{
+    var a = new AgentCollaborationPractice();
+
+    await a.Execute(
+        "gpt-3.5-turbo",
+        Environment.GetEnvironmentVariable("OPENAI_APIKEY", EnvironmentVariableTarget.User));
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
